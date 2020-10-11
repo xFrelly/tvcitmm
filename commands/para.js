9932406ba6c8a96a78330a907ed701c1eb0088e8
xFrelly <68738809+xFrelly@users.noreply.github.com>
2020-10-11 10:02:41 +0300

    Create para.js

---
+++ commands/para.js
@@ -0,0 +1,17 @@
+const discord = require('discord.js')
+const db = require('quick.db');
+
+module.exports.run = async(client , message , args) =>{
+    let kişi = message.mentions.members.first();
+
+ let bakiye = await db.fetch(`money_${message.guild.id}_${kişi.id}`)
+ if(bakiye === null)bakiye = 0;
+ message.channel.send(`${user.tag} , bakiyeniz şu anda **${bakiye}$** miktarında.`)
+}
+module.exports.config = {
+name : "bakiye",
+description : "",
+usage : "!bakiye",
+accesableby : "Members",
+alliases : "!para , !bal , !balance , !coins"
+}
