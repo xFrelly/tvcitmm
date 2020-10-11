b78973e40a19b97ba21a811044c2f08dd1c53921
xFrelly <68738809+xFrelly@users.noreply.github.com>
2020-10-11 11:19:27 +0300

    Update paraekle.js

--- commands/paraekle.js
+++ commands/paraekle.js
@@ -4,12 +4,11 @@ const db = require('quick.db')
 module.exports.run = async(client , message , args) =>{
     if(!message.member.roles.cache.get('764751515201175562'))
     message.channel.send('*Aynen bak şu an para ekledin .d*')
-    let kişi = message.mentions.users.first() || message.author;
 
     if(!args[1]) message.channel.send('*IQ seviyeni değil gireceğin miktarı yaz.*')
     if(isNaN(args[1])) message.channel.send('*Geçerli bir sayı yaz.*')
 
-
+    let kişi = message.mentions.users.first() || message.author;
     message.channel.send(`Başarıyla ${kişi} üyesine` + `**${args[1]}** kadar **$$$** eklendi.`)
     db.add(`money_${message.guild.id}_${kişi.id}` , args[1])
 }
