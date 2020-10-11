418404c1feef0f40d4a1ce7e9bb7d23ffd720c52
xFrelly <68738809+xFrelly@users.noreply.github.com>
2020-10-11 14:40:33 +0300

    Update rulet-bahis.js

--- commands/rulet-bahis.js
+++ commands/rulet-bahis.js
@@ -3,15 +3,15 @@ const db = require('quick.db')
 
 module.exports.run = async (client, message, args) => {
     const member = db.fetch(`money_${message.guild.id}_${message.author.id}`);
-
-    if (!isNaN(args[1])) message.channel.send('Geçerli bir miktar gir.');
+    let miktar = args.slice(1).join(' ');
+    if (!isNaN(miktar)) message.channel.send('Geçerli bir miktar gir.');
 
     let user = message.mentions.users.first() || message.author;
-    if (member < args[1]) {
+    if (member < miktar) {
         message.channel.send('Yetersiz bakiye...')
     }else{
-        message.channel.send(`${user} , rulete ${args[1]}$$$ para yatırdı.`)
-        db.subtract(`money_${message.guild.id}_${message.author.id}` , args[1])
+        message.channel.send(`${user} ` + `rulete ${miktar}$$$ para yatırdı.`)
+        db.subtract(`money_${message.guild.id}_${message.author.id}`, miktar)
     }
 
 
