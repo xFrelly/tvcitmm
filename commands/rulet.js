7baf1d80835ee19f611f0a71b2dbe867bcc74e35
xFrelly <68738809+xFrelly@users.noreply.github.com>
2020-10-11 13:25:38 +0300

    Update rulet.js

---
+++ commands/rulet.js
@@ -0,0 +1,13 @@
+const Dicord = require('discord.js')
+const db = require('quick.db')
+
+module.exports.run = async(client , message , args) => {
+ 
+
+module.exports.config = {
+    name : "",
+    description : "",
+    usage : "!",
+    accessableby : "",
+    aliases : [""],
+}
