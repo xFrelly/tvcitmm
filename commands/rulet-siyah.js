019fbdfa2c056f28e29e042717bff87aae936d48
xFrelly <68738809+xFrelly@users.noreply.github.com>
2020-10-11 14:01:54 +0300

    s

---
+++ commands/rulet-siyah.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+
+module.exports.run = async(client , message , args) => {
+
+}
+module.exports.config = {
+    name : "",
+    description : "",
+    usage : "" ,
+    accessableby : "",
+    aliases : "",
+}
--- commands/rulet.js
+++ commands/rulet.js
@@ -2,12 +2,23 @@ const Dicord = require('discord.js')
 const db = require('quick.db')
 
 module.exports.run = async(client , message , args) => {
+ const renk = '*!rulet-kırmızı*'
+ const ss = '=>||<='
+ const embed = new Dicord.MessageEmbed()
+
+ .setColor('RANDOM')
+ .setTitle('Ruleti Renklere göre oynamaya karar verdin işte kurallar...')
+ .setDescription('!rulet-bahis (gireceğin miktar) ile bir bahis yatır ve ardından kırmızı' + 
+ 'siyah' + 'veya yeşil renklerinden birini seç örnek olarak' , renk)
+ .addField('Başarılar...', ss)
+
+ message.channel.send(embed);
  
 }
 module.exports.config = {
-    name : "",
+    name : "rulet-renk",
     description : "",
-    usage : "!",
-    accessableby : "",
+    usage : "!rulet-renk",
+    accessableby : "Members",
     aliases : [""],
 }
