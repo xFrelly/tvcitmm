5b5083779a249fe09c0e31d57dc87fcea149edf4
xFrelly <68738809+xFrelly@users.noreply.github.com>
2020-10-11 10:23:08 +0300

    Update para.js

--- commands/para.js
+++ commands/para.js
@@ -4,7 +4,7 @@ const db = require('quick.db');
 module.exports.run = async(client , message , args) =>{
     let kişi = message.mentions.members.first();
 
- let bakiye = await db.fetch(`money_${message.guild.id}_${kişi.id}`)
+ let bakiye = await db.fetch(`money_${message.guild.id}_${kişi}`)
  if(bakiye === null)bakiye = 0;
  message.channel.send(`${user.tag} , bakiyeniz şu anda **${bakiye}$** miktarında.`)
 }
