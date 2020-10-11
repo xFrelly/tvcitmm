f7fe734f1395f8067fc764b8e57f6b947ace04ed
xFrelly <68738809+xFrelly@users.noreply.github.com>
2020-10-11 14:56:31 +0300

    Update rulet-bahis.js

--- commands/rulet-bahis.js
+++ commands/rulet-bahis.js
@@ -3,25 +3,26 @@ const db = require('quick.db')
 
 module.exports.run = async (client, message, args) => {
     const member = db.fetch(`money_${message.guild.id}_${message.author.id}`);
-    let miktar = args.slice(1).join(' ');
+    const miktar = args.slice(1).join(' ');
 
-
-    if (!isNaN(miktar))return message.channel.send('Geçerli bir miktar gir.');
-
-    let user = message.mentions.users.first() || message.author;
-    if (member < miktar) {
-        message.channel.send('Yetersiz bakiye...')
-    }else{
-        message.channel.send(`${user} ` + `rulete ${miktar}$$$ para yatırdı.`)
-        db.subtract(`money_${message.guild.id}_${message.author.id}`, miktar)
-    }
-
+        if(!miktar){
+            message.channel.send('*Paran yoksa oynamayalım*')
+        }
+        if(isNaN(miktar)){
+            message.channel.send('**Geçerli bir sayı gir.**')
+        }
+        if(miktar > member){
+            message.channel.send('Sende o kadar para yok.')
+        }else{
+            message.channel.send(`${user}` + ` **${miktar}$** kadar rulete bahis yatırdı! Bol Şans!!`)
+            db.subtract(`money_${message.guild.id}_${message.author.id}` , miktar)
+        }
 
 }
 module.exports.config = {
     name: "ruletbahis",
     description: "",
-    usage: "",
+    usage: "!ruletbahis",
     accessableby: "",
-    aliases: [""],
+    aliases: ["rulet-bahis"],
 }
