a3b923fbe635a6fee703bf6d32b2bc588a07e7a5
xFrelly <68738809+xFrelly@users.noreply.github.com>
2020-10-11 14:29:29 +0300

    Update rulet-bahis.js

--- commands/rulet-bahis.js
+++ commands/rulet-bahis.js
@@ -1,26 +1,25 @@
 const Discord = require('discord.js')
 const db = require('quick.db')
 
-module.exports.run = async(client , message , args) => {
+module.exports.run = async (client, message, args) => {
     const kisi = db.fetch(`money_${message.guild.id}_${message.author.id}`);
 
-    if(!isNaN(args[1])) message.channel.send('Geçerli bir miktar gir.');
-    if(!args[1]) message.channel.send('Paran yoksa oynamayalım...');
+    if (!isNaN(args[1])) message.channel.send('Geçerli bir miktar gir.');
 
     let user = message.mentions.users.first() || message.author;
-    if(args[1] < kisi){
-    message.channel.send(`${user} kişisi rulete ${args[1]}$$$ bahis yatırdı!`)
-    db.subtract(`money_${message.guild.id}_${message.author.id}` , args[1])
-    }else{
+    if (args[1] > kisi) {
+        message.channel.send(`${user} kişisi rulete ${args[1]}$$$ bahis yatırdı!`)
+        db.subtract(`money_${message.guild.id}_${message.author.id}`, args[1])
+    } else {
         message.channel.send('Yetersiz bakiye.')
     }
-    
+
 
 }
 module.exports.config = {
-    name : "rulet-bahis",
-    description : "",
-    usage : "" ,
-    accessableby : "",
-    aliases : [""],
+    name: "rulet-bahis",
+    description: "",
+    usage: "",
+    accessableby: "",
+    aliases: [""],
 }
