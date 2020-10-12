6d7e87f3b56bd6bea6125ce2c1b4cb8947691310
xFrelly <68738809+xFrelly@users.noreply.github.com>
2020-10-12 18:40:48 +0300

    addd

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const command = require('./command/command')
 const db = require('quick.db')
+const isValidCommand = (message, cmdName) => message.content.toLowerCase().startsWith(PREFİX + cmdName);
 
 client.on('ready', () =>{
     console.log(`Botun ${client.user.tag} aktif!`)
@@ -94,6 +95,32 @@ command(client, 'tükür', (message) => {
     )
   }
 });
+
+client.on('message' , message =>{
+  const miktar = args[0]
+  const kisi = db.fetch(`money_${message.guild.id}_${message.author.id}`)
+  const user = message.users.mentions.first() ||message.author;
+  const çark = () => Math.floor(Math.random() * 3) + 1
+  if(isValidCommand(message , 'rulet bahis')){
+    if(isNaN(miktar)) return message.channel.send('Geçerli bir sayı gir.')
+    if(miktar > kisi){ message.channel.send('Yetersiz bakiye.')
+  }else{
+    message.channel.send(`${user}` + ` kişisi **${miktar}$$** kadar para yatırdı. Şimdi ne yapacaksın? Kırmızı mı? Siyah mı? Yeşil mi?`)
+  }
+  }
+  if(isValidCommand(message , 'rulet kırmızı')){
+    message.channel.send('Çark çevrildi ve gelen renk...' + çark)
+    if(message.content === '3'){
+      await message.delete();
+      message.channel.send('Kırmızı!')
+      message.channel.send('Tebrikler ! Kırmızı geldi!')
+      db.add(`money_${message.guild.id}_${message.author.id}` , miktar)
+    }else{
+      message.channel.send('Tüh! Kırmızı gelmedi !')
+      db.subtract(`money_${message.guild.id}_${message.author.id}` , miktar)
+    }
+  }
+})
 
 
 
